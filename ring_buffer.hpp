6cd4baa7ba6c8872b1877e23bf1335ae77beac8f
sidprs <sidprasad2004@gmail.com>
2026-01-20 18:38:49 -0700

    rule of 5

---
+++ ring_buffer.hpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+
+template<typename T>
+class RingBuffer{
+  private:
+    struct Slot{
+      T value_;
+      int index_;
+    };
+  
+  Slot* buff_;
+  size_t capacity_;
+  int size_; 
+  int head_;
+  int tail_;  
+  
+  public:
+  RingBuffer(size_t capacity) : size_(0), capacity_(capacity),
+    head_(0), tail_(0)
+  {   
+    buff_ = new Slot[capacity_];
+  }
+
+  void push(T value){
+    head_ = (head_ + 1) % capacity_;
+    if(size_ == capacity_){
+      tail_ = (tail_ + 1) % capacity_;
+    }
+    else{
+      size_++;
+    }
+    buff_[head_].value_ = value;  
+    buff_[head_].index_ = size_;
+  }
+
+  T pop(){
+    if(size_ == 0 ){
+      throw std::runtime_error("[pop] eror size == 0");
+    }
+    T val = buff_[tail_].value;
+    tail_ = (tail_ + 1) % capacity_;
+    size_--;
+    return val;
+  } 
+  
+  const T& front() const{
+    if(size_ == 0){
+      throw std::runtime_error("[pop] eror size == 0");
+    }
+    T curr = buff_[head_].value;
+    return curr;
+  }
+  const T& back() const{
+    if(size_ == 0){
+      throw std::runtime_error("[pop] eror size == 0");
+    }
+    int last = (head_ - 1 + capacity_) % capacity_;
+    T curr = buff_[last].value;
+    return curr;
+  }
+  
+  RingBuffer& operator=(const RingBuffer&) = delete;
+  RingBuffer(const RingBuffer&) = delete;
+
+
+  RingBuffer(RingBuffer&& other) noexcept 
+    :buff_(other.buff_), head_(other.head_), tail_(other.tail_),
+     size_(other.size_), capacity_(other.capacity_)
+  {
+    other.buff_ = nullptr;
+    other.size = 0;
+    other.capacity_ = 0;
+  }
+  
+  RingBuffer& operator=(RingBuffer&& other){
+
+  }
+
+
+};
+
+
+int main(int argc, char* argv[]){
+    
+
+  std::cout << "hello" << std::endl;
+  return 0;
+}
